de53fe8743e3f0a0bfc2c9cfec3e88ae05411c6d
collin <geniuscollin@gmail.com>
2022-01-26 13:13:02 -0600

    added unsubscribe button and functionality

--- src/apimanager/subscriptionFetches.js
+++ src/apimanager/subscriptionFetches.js
@@ -20,3 +20,7 @@ export const getUsersSubscriptions = (id) => {
     return fetch(`http://localhost:8088/subscriptions?user_id=${id}`)
     .then(res => res.json())
 }
+
+export const deleteSubscription = (id) => {
+    return fetch(`http://localhost:8088/subscriptions/${id}`, {method: "DELETE"})
+}
