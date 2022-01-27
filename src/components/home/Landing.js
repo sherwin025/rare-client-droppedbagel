9af333da354566f3d288e96c9e629b0f0166e84b
Sherwin Vargas <svargas025@gmail.com>
2022-01-27 10:18:16 -0600

    postcss

--- src/components/home/Landing.js
+++ src/components/home/Landing.js
@@ -4,8 +4,11 @@ import { Link } from "react-router-dom"
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
 import { getSubscriptions, getUsersSubscriptions } from "../../apimanager/subscriptionFetches"
 import { GetPosts } from "../posts/PostManager"
+import "../posts/MyPost.css"
+import TrashIcon from '../comments/trash.svg'
 
-export const Landing = () => { 
+
+export const Landing = () => {
     const [subs, setSubs] = useState([])
     const [posts, setPosts] = useState([])
 
@@ -13,12 +16,12 @@ export const Landing = () => {
         GetPosts().then(setPosts)
         getUsersSubscriptions(+localStorage.getItem('token')).then(setSubs)
 
-    },[])
+    }, [])
 
     const usersSubs = subs.map((sub) => {
         return sub.follower_id
     })
-    
+
 
     const filteredPosts = () => posts.filter((post) => {
         for (const userSub of usersSubs) {
@@ -35,22 +38,35 @@ export const Landing = () => {
 
 
 
-    return (
+    return (<>
+        <div className="newpostbutton">
+            <button>New Post</button>
+        </div>
+        <div className="mypostlist">
+            {
+                !!subs.length > 0
+                    ? filteredPosts().map((each) => {
+                        return <div key={each.id} className="indpost">
+                            <div className="posthead">
+                                <div><Link to={`./posts/${each.id}`}> {each.title}</Link></div>
+                                <div>Publication Date: {new Date(each.publication_date).toLocaleDateString()}</div>
+                            </div>
 
-        <>
+                            <div className="imageposturl">IMG:{each.image_url}</div>
+                            <div className="postfoot">
+                                <div>
+                                    <div> Author: {each.user?.first_name} {each.user?.last_name}</div>
+                                </div>
 
-        {
-            !!subs.length > 0
-            ? filteredPosts().map((each) => {
-                return <div key={each.id} className="indpost">
-                <div><Link to={`./posts/${each.id}`}> {each.title}</Link></div>
-                <div>{each.user?.last_name} {each.user?.first_name}</div>
-                <div>{each.category?.label}</div>
-                <div>{each.content}</div>
-                </div>
-            })
-            : <h1>Subscribe to authors to curate your personal homepage</h1>
-        }
-        </>
-    )
+                                <div className="rightfoot">
+                                    <button onClick={() => { history.push(`/editPost/${each.id}`) }}>edit</button>
+                                    <button onClick={() => deletepost(each.id)}><img src={TrashIcon} style={{ height: "1.25rem" }} ></img></button>
+                                </div>
+                            </div>
+                        </div>
+                    })
+                    : <h1>Subscribe to authors to curate your personal homepage</h1>
+            }
+        </div>
+    </>)
 }
