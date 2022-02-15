15f8ee097a256990651bf5161d09ca3ea1769eac
Sherwin Vargas <svargas025@gmail.com>
2022-02-15 13:54:43 -0600

    fixed auth and userprofiles

--- src/components/Users/userManager.js
+++ src/components/Users/userManager.js
@@ -1,9 +1,13 @@
 export const getAllUsers = () => {
-    return fetch('http://localhost:8000/users')
+    return fetch('http://localhost:8000/users', {
+        headers: { "Authorization": `Token ${localStorage.getItem("token")}` }
+    })
     .then(res => res.json())
 }
 
 export const getSingleUser = (id) => {
-    return fetch(`http://localhost:8000/users/${id}`)
+    return fetch(`http://localhost:8000/users/${id}`, {
+        headers: { "Authorization": `Token ${localStorage.getItem("token")}` }
+    })
     .then(res => res.json())
 }
